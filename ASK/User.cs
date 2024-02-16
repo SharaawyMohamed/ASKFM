2863715003a97a198387e054b4b02580727fa68d
Sharawy Mohamed <142440242+SharaawyMohamed@users.noreply.github.com>
2024-02-16 17:15:51 +0200

    Update User.cs

--- ASK/User.cs
+++ ASK/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,16 +14,6 @@ namespace ASK
         internal string UserName { get; set; }
         internal string Email { get; set; }
         internal int Anonymous { get; set; } = 0;
-        //constructor if sign in(username, password)
-
-        //sign up
-        //internal User(int _id,string _name,string _password,string _username, string _email)
-        //{
-        //    Id= _id;
-        //    Name= _name;
-        //    Password = _password;
-        //    UserName= _username;
-        //    Email=_email;
-        //}
+        
     }
 }
