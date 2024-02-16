c900c59928e36a9cd77c9cbea2931954e40a0834
Sharawy Mohamed <142440242+SharaawyMohamed@users.noreply.github.com>
2024-02-16 17:11:38 +0200

    Update QuestionManage.cs

--- ASK/QuestionManage.cs
+++ ASK/QuestionManage.cs
@@ -1,35 +1,109 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ASK
 {
-    internal class QuestionManage
+    internal static class QuestionManage
     {
-       // Operations
-        internal QuestionManage()
+        public static void PrintParentQ(Question quest, int Anonymo = 0, bool FromMe = false)
         {
-
+            string Anon = Anonymo == 1 ? "" : " !AQ";
+            string Fromme = !FromMe ? $"From User ID({quest.FromId})" : Anon;
+            string Ans = string.IsNullOrEmpty(quest.Answer) ? "No Answer For this Question" : quest.Answer;
+            Console.WriteLine($"Question ID({quest.QId}){Fromme} To User ID({quest.ToId}) Question: {quest.Body} "); // from & to
+            Console.WriteLine($"Answer: {Ans}");
         }
-        internal static void PrintQ(Question question)
+        public static void PrintThreadQ(Question quest, int Anonymo = 0, bool FromMe = false)
         {
-            Console.WriteLine($"From user ({question.FromId}) To user ({question.ToId}) Question ID :({question.QId})  {question.Body} ");
-           
+            string Anon = Anonymo == 1 ? "" : " !AQ";
+            string Fromme = !FromMe ? $"From User ID({quest.FromId})" : Anon;
+            string Ans = string.IsNullOrEmpty(quest.Answer) ? "No Answer For this Question" : quest.Answer;
 
+            Console.WriteLine($"Thread :   Question ID({quest.QId}) {Fromme} To User ID({quest.ToId}) {quest.Body}");
+            Console.WriteLine($"Thread :   Answer:   {Ans}");
         }
-        internal static void PrintTHread(List<Question> ThridList)
+        public static void Delete(int id)
         {
-            foreach (Question i in ThridList)
+            try
             {
-                Console.Write("   Thread:");
-                //   PrintQ(i);
+                string query = "Delete FROM Question WHERE Thread = @ID";
+                SqlConnection connection = new SqlConnection(DB.Source);
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                command.Parameters.AddWithValue("@ID", id);
+                command.ExecuteScalar();
+
+                query = "Delete FROM Question WHERE Id = @ID";
+                command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ID", id);
+                command.ExecuteScalar();
+                connection.Close();
+                Console.WriteLine("Done!");
             }
-        }
-        internal void printAns(string Answer)
+            catch (Exception)
+            {
+                Console.WriteLine("You Can't delete this question because have multi thread");
+            }
+
+
+        }// user recursion to Delete Multi Thread
+        public static void AnswerQ(int id)
         {
-            Console.WriteLine(Answer);
+            Console.Write("Enter Question Answer: ");
+            string Ans = Console.ReadLine();
+            do
+            {
+                if (!string.IsNullOrEmpty(Ans)) break;
+                Console.WriteLine("Invalid Answer");
+                Ans = Console.ReadLine();
+            } while (true);
+
+            string sql = "Update [Question] set Answer=@ans where Id=@id ";
+            SqlConnection con = new SqlConnection(DB.Source);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@ans", Ans);
+            cmd.ExecuteScalar();//System.InvalidOperationException: 'ExecuteNonQuery requires an open and available Connection. The connection's current state is 
+            con.Close();
+            Console.WriteLine("Done!\n");
         }
+        public static void AskQ(string quest, int FromId, int ToId, int thread)
+        {
+            string sql = "INSERT INTO Question(Body, [FromId], [ToId],[Thread]) VALUES (@body, @from, @to,@thread)";
+            SqlConnection con = new SqlConnection(DB.Source);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.Add(new SqlParameter("@body", quest));
+            cmd.Parameters.Add(new SqlParameter("@from", FromId));
+            cmd.Parameters.Add(new SqlParameter("@to", ToId));
+            cmd.Parameters.Add(new SqlParameter("@thread", thread));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Console.WriteLine("Done!\n");
+        }
+        public static void AskQ(string quest, int FromId, int ToId)
+        {
+            string sql = "INSERT INTO Question(Body, [FromId], [ToId]) VALUES (@body, @from, @to)";
+            SqlConnection con = new SqlConnection(DB.Source);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.Add(new SqlParameter("@body", quest));
+            cmd.Parameters.Add(new SqlParameter("@from", FromId));
+            cmd.Parameters.Add(new SqlParameter("@to", ToId));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Console.WriteLine("Done!\n");
+        }
+
     }
+
 }
